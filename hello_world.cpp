f6a2445913227742fc5bda1f9e3295292de81dd0
ejsanchez7 <54457453+ejsanchez7@users.noreply.github.com>
2023-11-10 15:39:06 -0600

    Prueba de commit

---
+++ hello_world.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+float promedio(int a, int b, int c) {
+    return (a + b + c) / 3;
+}
+
+int main() {
+    int a;
+    int b;
+    int c;
+
+    cout << "Escribe el primer número: ";
+    cin >> a;
+    cout << "Escribe el segundo número: ";
+    cin >> b;
+    cout << "Escribe el tercer número: ";
+    cin >> c;
+
+    cout << "El promedio es: " << promedio(a, b, c) << endl;
+
+    return 0;
+}
